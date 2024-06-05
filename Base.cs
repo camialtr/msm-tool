6456617aa474774ee2aa1ebb7fc8976897b06c1d
Cami <camialtr.personal@gmail.com>
2024-06-05 04:31:03 -0300

    Code readability optimization

---
+++ Base.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scoring_analysis
+{
+    internal class Base
+    {
+        public static readonly string newLine = Environment.NewLine;
+        public static readonly string version = "0.0.1a";
+#if (DEBUGX86 || RELEASEX86)
+        public static readonly string preset = "JDN API";
+        public static readonly string[] commands = new string[2]
+        {
+            "  [0] Example Option to JDNOW",
+            "  [1] Example Option to JDNOW"
+        };
+#elif (DEBUGX64 || RELEASEX64)
+        public static readonly string preset = "UAF API";
+        public static readonly string[] commands = new string[2]
+        {
+            "  [0] Example Option to UAF",
+            "  [1] Example Option to UAF"
+        };
+#endif
+        public static readonly string header = "Just Dance Scoring APIs Analyzer | Created by Cami" + newLine + $"Version: {version}" + newLine + $"Preset: {preset}" + newLine;
+    }
+
+    public struct ScoringRecorder
+    {
+        public string mapName;
+        public int coachID;
+        public float recordedFinalScore;
+        public List<Move> moves;
+        public List<RecordedAccData> recordedAccData;
+        public List<RecordedScore> recordedScore;
+    }
+
+    public struct Move
+    {
+        public float time;
+        public float duration;
+        public string name;
+        public int goldMove;
+    }
+
+    public struct RecordedAccData
+    {
+        public float accX;
+        public float accY;
+        public float accZ;
+        public float mapTime;
+    }
+
+    public struct RecordedScore
+    {
+        public string feedback;
+        public float addedScore;
+        public float totalScore;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,65 +6,15 @@ using MoveSpaceWrapper;
 
 namespace scoring_analysis
 {
-    internal unsafe class Program
+    internal unsafe class Program : Base
     {
-        private static readonly string newLine = Environment.NewLine;
-        private static readonly string version = "0.0.1a";
-#if (DEBUGX86 || RELEASEX86)
-        private static readonly string preset = "UAF API";
-#elif (DEBUGX64 || RELEASEX64)
-        private static readonly string preset = "JDN API";
-#endif
-        private static readonly string header = "Just Dance Scoring APIs Analyzer | Created by Cami" + newLine + $"Version: {version}" + newLine + $"Preset: {preset}" + newLine;
-
         static void Main(string[] args)
         {
             Console.WriteLine(header);
             Console.WriteLine($"Select an option below: {newLine}");
-            string[] commands = new string[2]
-            {
-                "[0] Example Option",
-                "[1] Example Option"
-            };
-            foreach (string command in commands)
-            {
-                Console.WriteLine(command.PadLeft(20));
-            }
+            foreach (string command in commands) Console.WriteLine(command);
             Console.Write($"{newLine}Type code: ");
             Console.Read();
         }
-    }
-
-    public struct ScoringRecorder
-    {
-        public string mapName;
-        public int coachID;
-        public float recordedFinalScore;
-        public List<Move> moves;
-        public List<RecordedAccData> recordedAccData;
-        public List<RecordedScore> recordedScore;
-    }
-
-    public struct Move
-    {
-        public float time;
-        public float duration;
-        public string name;
-        public int goldMove;
-    }
-
-    public struct RecordedAccData
-    {
-        public float accX;
-        public float accY;
-        public float accZ;
-        public float mapTime;
-    }
-
-    public struct RecordedScore
-    {
-        public string feedback;
-        public float addedScore;
-        public float totalScore;
     }
 }
