4952a5b67f325f38f10db98a08034ec6b1ff620e
Cami <camialtr.personal@gmail.com>
2024-06-05 03:17:06 -0300

    Basic interface implementation started
    It's only the beginning, but i'm too tired to program today.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,49 @@ namespace scoring_analysis
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Just Dance Scoring Analysis by Cami");
+            Console.WriteLine("Version: 0.0.1a");
+#if (DEBUGX86 || RELEASEX86)
+            Console.WriteLine("Preset: UAF API");
+#elif (DEBUGX64 || RELEASEX64)
+            Console.WriteLine("Preset: JDN API");
+#endif
+            Console.WriteLine();
+            Console.WriteLine("Select one of the options:");
+            Console.WriteLine("");
         }
+    }
+
+    public struct ScoringRecorder
+    {
+        public string mapName;
+        public int coachID;
+        public float recordedFinalScore;
+        public List<Move> moves;
+        public List<RecordedAccData> recordedAccData;
+        public List<RecordedScore> recordedScore;
+    }
+
+    public struct Move
+    {
+        public float time;
+        public float duration;
+        public string name;
+        public int goldMove;
+    }
+
+    public struct RecordedAccData
+    {
+        public float accX;
+        public float accY;
+        public float accZ;
+        public float mapTime;
+    }
+
+    public struct RecordedScore
+    {
+        public string feedback;
+        public float addedScore;
+        public float totalScore;
     }
 }
