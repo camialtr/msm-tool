9a450eea660959aa7405838551c58ade25ab3a9d
Cami <camialtr.personal@gmail.com>
2024-06-05 03:07:05 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿#if (DEBUGX86 || RELEASEX86)
+using JDNow;
+#elif (DEBUGX64 || RELEASEX64)
+using MoveSpaceWrapper;
+#endif
+
+namespace scoring_analysis
+{
+    internal unsafe class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
